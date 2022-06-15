125cf12bf78d9fc34f6c83330d02592cf5bd922f
Vitor <moutimg@gmail.com>
2022-06-15 13:59:02 -0300

    refactor: refatora condicionais para deixar codigo mais limpo

--- src/middlewares/verifyBodyLogin.js
+++ src/middlewares/verifyBodyLogin.js
@@ -1,20 +1,12 @@
 const verifyEmail = (email) => {
   const regex = /\S+@\S+\.\S+/;
 
-  if (!email) {
-    throw new Error(JSON.stringify({ status: 400, message: 'Some required fields are missing' }));
-  }
-
   if (!regex.test(email)) {
     throw new Error(JSON.stringify({ status: 400, message: 'Email in incorrect format' }));
   }
 };
 
 const verifyPassword = (password) => {
-  if (!password) {
-    throw new Error(JSON.stringify({ status: 400, message: 'Some required fields are missing' }));
-  }
-
   if (password.length < 3) {
     throw new Error(JSON.stringify(
       { status: 400, message: 'password must be 3 or more characters' },
@@ -24,6 +16,10 @@ const verifyPassword = (password) => {
 
 const verifyBodyLogin = (req, res, next) => {
   const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new Error(JSON.stringify({ status: 400, message: 'Some required fields are missing' }));
+  }
 
   verifyEmail(email);
   verifyPassword(password);
