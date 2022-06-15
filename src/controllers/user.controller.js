6a532b15e704d053fe2e675b0b7428d1b2998e8b
Vitor <moutimg@gmail.com>
2022-06-15 15:15:11 -0300

    feat: cria funcao na camada de controller para listar todos os usuarios da tabela Users

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,6 +6,13 @@ const createUser = async (req, res) => {
   res.status(201).json({ token });
 };
 
+const getUsers = async (req, res) => {
+  const users = await service.getUsers();
+
+  res.status(200).json(users);
+};
+
 module.exports = {
   createUser,
+  getUsers,
 };
