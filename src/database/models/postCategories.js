d29fe227c80c5ece3b5c40285441ec0b8794c7df
Vitor <moutimg@gmail.com>
2022-06-15 01:24:31 -0300

    feat: cria arquivo da camada de Model para a PostCategories

---
+++ src/database/models/postCategories.js
@@ -0,0 +1,20 @@
+const postCategories = (sequelize, DataTypes) => {
+  const tablePostCategories = sequelize.define("PostCategories", {
+    userId: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER,
+  }, { timestamps: false });
+
+  tablePostCategories.associate = (models) => {
+    tablePostCategories.belongsTo(models.User, {
+      foreignKey: 'userId', as: 'user'
+    });
+
+    tablePostCategories.belongsTo(models.Categories, {
+      foreignKey: 'categoryId', as: 'category'
+    });
+  };
+
+  return tablePostCategories;
+}
+
+module.exports = postCategories;
