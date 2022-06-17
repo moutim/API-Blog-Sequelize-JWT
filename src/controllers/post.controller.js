1941572e3b6d7e6003a510826774de1c79bce3e4
Vitor <moutimg@gmail.com>
2022-06-17 15:26:59 -0300

    feat: cria funcao na camada de controller para listar o post, categoria post e o usuario que postou

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -6,6 +6,14 @@ const createPost = async (req, res) => {
   res.status(201).json(post);
 };
 
+const getPosts = async (req, res) => {
+  const posts = await service.getPosts();
+  console.log(posts.BlogPost);
+
+  res.status(200).json(posts);
+};
+
 module.exports = {
   createPost,
+  getPosts,
 };
