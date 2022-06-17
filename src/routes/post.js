ff44bdc1a18c66a206fdd58abc282772bad015e2
Vitor <moutimg@gmail.com>
2022-06-17 15:27:45 -0300

    feat: cria rota GET '/post' para listar o post, categoria o usuario

--- src/routes/post.js
+++ src/routes/post.js
@@ -6,6 +6,8 @@ const controller = require('../controllers/post.controller.js');
 
 const middlewares = require('../middlewares');
 
+routes.get('/', controller.getPosts);
+
 routes.post('/', middlewares.verifyBodyPost, controller.createPost);
 
 module.exports = routes;
