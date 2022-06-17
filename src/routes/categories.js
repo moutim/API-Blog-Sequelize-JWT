8642899dbe0f725f9160733e954ff1c8a49b3af4
Vitor <moutimg@gmail.com>
2022-06-16 22:10:21 -0300

    feat: cria as rotas GET, POST, PUT e DELETE para a tabela Categories

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -6,6 +6,14 @@ const controller = require('../controllers/categories.controller');
 
 const middlewares = require('../middlewares');
 
+routes.get('/', controller.getCategories);
+
+routes.get('/:id', controller.getCategory);
+
 routes.post('/', middlewares.verifyBodyCategories, controller.createCategory);
+
+routes.put('/:id', middlewares.verifyBodyCategories, controller.updateCategory);
+
+routes.delete('/:id', controller.deleteCategory);
 
 module.exports = routes;
