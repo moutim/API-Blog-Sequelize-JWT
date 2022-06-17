e578ac4500df34c2f3fe922e6f936f9eda3c2371
Vitor <moutimg@gmail.com>
2022-06-16 22:09:43 -0300

    feat: cria funcoes na camada de service para buscar, deletar e atualizar categorias na tabela Categories

--- src/services/categories.service.js
+++ src/services/categories.service.js
@@ -10,6 +10,54 @@ const createCategory = async (name) => {
   }
 };
 
+const getCategories = async () => {
+  const categories = await Category.findAll();
+
+  if (!categories) {
+    throw new Error(JSON.stringify({ status: 500, message: 'Categories not found' }));
+  }
+
+  return categories;
+};
+
+const getCategory = async (id) => {
+  const category = await Category.findByPk(id);
+
+  if (!category) {
+    throw new Error(JSON.stringify({ status: 500, message: 'Category does not exist' }));
+  }
+
+  return category;
+};
+
+const updateCategory = async (body, id) => {
+  await getCategory(id);
+
+  try {
+    const category = await Category.update(body, { where: { id } });
+
+    return category;
+  } catch (e) {
+    throw new Error(JSON.stringify({ status: 500, message: e.message }));
+  }
+};
+
+const deleteCategory = async (id) => {
+  await getCategory(id);
+
+  try {
+    const category = await Category.destroy({ where: { id } });
+
+    return category;
+  } catch (e) {
+    throw new Error(JSON.stringify({ status: 500, message: e.message }));
+  }
+};
+
 module.exports = {
   createCategory,
+  getCategory,
+  getCategories,
+  updateCategory,
+  deleteCategory,
 };
