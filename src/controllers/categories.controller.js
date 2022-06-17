774402f733d53c750892721fc3155ddbecf7adae
Vitor <moutimg@gmail.com>
2022-06-16 22:09:28 -0300

    feat: cria funcoes na camada de controller para buscar, deletar e atualizar categorias na tabela Categories

--- src/controllers/categories.controller.js
+++ src/controllers/categories.controller.js
@@ -8,6 +8,40 @@ const createCategory = async (req, res) => {
   res.status(201).json(category);
 };
 
+const getCategories = async (req, res) => {
+  const categories = await service.getCategories();
+
+  res.status(200).json(categories);
+};
+
+const getCategory = async (req, res) => {
+  const { id } = req.params;
+
+  const category = await service.getCategory(id);
+
+  res.status(200).json(category);
+};
+
+const updateCategory = async (req, res) => {
+  const { id } = req.params;
+
+  await service.updateCategory(req.body, id);
+
+  res.status(200).json({ id, ...req.body });
+};
+
+const deleteCategory = async (req, res) => {
+  const { id } = req.params;
+
+  await service.deleteCategory(id);
+
+  res.status(204).end();
+};
+
 module.exports = {
   createCategory,
+  getCategories,
+  getCategory,
+  updateCategory,
+  deleteCategory,
 };
